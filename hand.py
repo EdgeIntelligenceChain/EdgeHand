70afcc00f5754e8c9ae30734ab28f7b549b2e5c2
Jin <jlxufly@gmail.com>
2019-03-14 01:58:48 +0800

    client example in getting balance

---
+++ hand.py
@@ -0,0 +1,95 @@
+import binascii
+import time
+import json
+import hashlib
+import threading
+import _thread
+import logging
+import socketserver
+import socket
+import random
+import os
+from functools import lru_cache, wraps
+from typing import (
+    Iterable, NamedTuple, Dict, Mapping, Union, get_type_hints, Tuple,
+    Callable)
+from ds.Transaction import Transaction
+from ds.Block  import Block
+from ds.UnspentTxOut import UnspentTxOut
+from utils.Errors import BlockValidationError
+from utils.Utils import Utils
+
+from p2p.Message import Message
+
+from p2p.Peer import Peer
+from ds.TxIn import TxIn
+from ds.TxOut import TxOut
+from ds.MerkleNode import MerkleNode
+
+from p2p.Message import Message
+from p2p.Message import Actions
+
+from wallet.Wallet import Wallet
+from params.Params import Params
+
+def with_lock(lock):
+    def dec(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            with lock:
+                return func(*args, **kwargs)
+        return wrapper
+    return dec
+
+gs = dict()
+gs['Block'], gs['Transaction'], gs['UnspentTxOut'], gs['Message'], gs['TxIn'], gs['TxOut'], gs['Peer'] = globals()['Block'], \
+                    globals()['Transaction'], globals()['UnspentTxOut'], globals()['Message'], \
+                    globals()['TxIn'], globals()['TxOut'], globals()['Peer']
+
+chain_lock = threading.RLock()
+
+
+wallet = Wallet.init_wallet('mywallet.dat')
+peerList = Peer.init_peers(Params.PEERS_FILE)
+
+@with_lock(chain_lock)
+def getBalance4Addr(addr: str) -> int:
+    peer = random.sample(peerList, 1)[0]
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("",0))
+    s.listen(1)
+    port = s.getsockname()[1]
+    s.close()
+    message = Message(Actions.Balance4Addr, addr, port)
+    print(message, peer, port)
+    Utils.send_to_peer(message, peer)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', port))
+    s.listen(True)
+    conn, addr = s.accept()
+
+    timeout = time.time() + 60
+
+    while True and time.time() < timeout:
+        if addr[0] == peer[0]:
+            data = conn.recv(1024)[4:]
+            #print('###', data)
+            if data:
+                break
+        else:
+            pass
+    conn.close()
+
+    message = Utils.deserialize(data.decode(), gs)
+    return message.data
+
+
+
+
+
+balance = getBalance4Addr(wallet.my_address)
+
+print('balance is: ', balance)
+
