0bb6d87aa56094c48c8d4198cd4c995cec3c87a7
Jin <jlxufly@gmail.com>
2019-03-25 22:09:28 +0800

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from EdgeHand import EdgeHand
 
+#初始化钱包,默认本地钱包为mywallet.dat
 edgeHand = EdgeHand()
+
+#查询钱包余额，当不传入地址时，默认查询初始化钱包的余额
 balance = edgeHand.getBalance4Addr()
 
-print('balance is: ', balance)
-result = edgeHand.sendTransaction('1BZG4Tq1XgiJtUEBcnP9UUa7zjmfVuZiQa', 110)
-print('sendTransaction result: ', result)
+#发送交易
+txn = edgeHand.sendTransaction('1NY36FKZqM97oEobfCewhUpHsbzAUSifzo', 1000000000)
+
+#查询交易状态
+edgeHand.getTxStatus(txn)
+
+# 输出样例：
+# #185000000000# in address b'1M32gppnnKfCcedHq3weaAagKU7Ppt6KFD'
+# #37# utxo in address b'1M32gppnnKfCcedHq3weaAagKU7Ppt6KFD'
+# txn 8735269dc5665dea105266ad080a0df003cf3396f9ed296f7f020367f3962ef8 found in_mempool
